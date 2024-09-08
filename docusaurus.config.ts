4ca2db58fc43b58254dd90fba6254e0e431fce4a
niko0xdev <niko0xdev@gmail.com>
2024-09-08 17:46:57 +0700

    reupdate shared image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/logo.jpg",
     navbar: {
       title: "Niko Blog",
       logo: {
